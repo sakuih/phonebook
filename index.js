4852b771528da3c983d6f3061f2842b5fcb350de
taco <taco@taco.taco>
2024-08-12 15:04:23 +0300

    3.21 3.22 part3 complete

--- index.js
+++ index.js
@@ -18,25 +18,8 @@ morgan.token('body', req => {
 
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
-//const allowedOrigins = ['http://localhost:3001']
-//app.use(cors())
-app.use(cors({
-  origin: 'https://fsopen-phonebook-6ih1.onrender.com/',
-  header: ['Content-Type'],
-  credentials: true,
-}))
-  
-/*
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true)
-    } else {
-      callback(new Error('notAllowed by CORS'))
-    }
-  },
-  credentials: true,
-}))
-*/
+
+app.use(cors())
 app.use(bodyParser.json())
 app.use(express.static('build'))
 
